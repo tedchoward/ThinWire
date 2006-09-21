3983ff8cf4d9a8ede1088e8858264b4d868cb754
Joshua Gertzen <me@joshgertzen.com>
2006-09-21 23:35:59 +0000

    * Corrected an issue with TextFields that have the inputHidden property   set to true.  Previously, inputHidden fields would display their text   to the end user if they hovered over the field.  Now the auto tooltip   feature will not activate on inputHidden fields.

--- src/thinwire/render/web/resources/TextField.js
+++ src/thinwire/render/web/resources/TextField.js
@@ -5,13 +5,14 @@
 var tw_TextField = tw_BaseText.extend({
     construct: function(id, containerId, props) {
         this.$.construct.apply(this, [["div", "input", props.inputHidden ? "password" : "text"], "textField", id, containerId, "editMask"]);
+        if (props.inputHidden) this._useToolTip = false;
         delete props.inputHidden;
         this.init(-1, props);        
     },
 
     setInputHidden: function(inputHidden) {
-        new tw_TextField(this._id, this._parent._id, {"x": parseInt(this._box.style.left), "y": parseInt(this._box.style.top), 
-            "width": this._width, "height": this._height, "visible": this.isVisible(),
+        new tw_TextField(this._id, this._parent._id, {"x": this.getX(), "y": this.getY(), 
+            "width": this.getWidth(), "height": this.getHeight(), "visible": this.isVisible(),
             "enabled": this.isEnabled(), "text": this._box.value, "editMask": this._editMask,
             "alignX": this._editor.style.textAlign, "inputHidden": inputHidden});
         _parent.removeComponent(this);
