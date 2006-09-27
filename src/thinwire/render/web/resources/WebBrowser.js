c24257e44749fa8227a0769915ccecd85522ef47
Ted C. Howard <ted@tedchoward.com>
2006-09-27 16:55:14 +0000

    [09/27/06] * Corrected bug 1537456 - WebBrowser component in IE fails to display some pages

--- src/thinwire/render/web/resources/WebBrowser.js
+++ src/thinwire/render/web/resources/WebBrowser.js
@@ -24,27 +24,26 @@
  #VERSION_HEADER#
  */
 var tw_WebBrowser = tw_BaseBrowserLink.extend({
-    _location: "",
     
     construct: function(id, containerId, props) {
         this.$.construct.apply(this, ["iframe", "webBrowser", id, containerId]);
-        this._box.style.overflow = "auto";        
+        this._box.style.overflow = "auto";
         this._fontBox = null;
-        this._setLocationTimer = this._setLocationTimer.bind(this);
+        this._borderBox = null;
         var s = this._box.style;
-        this.init(-1, props);        
+        if (tw_isIE) this._box.allowTransparency = true;
+        this.init(-1, props);
+        s.backgroundColor = "window";
     },
     
     setLocation: function(location) {
-        this._location = location;
-        setTimeout(this._setLocationTimer, 100);
+        //NOTE: this line throws an error in firefox, but it still works.
+        if (location != "") location = tw_BaseBrowserLink.expandLocation(location);
+        this._box.src = location;
     },
     
-    _setLocationTimer: function() {
-        var location = this._location;
-        if (location != "") location = tw_BaseBrowserLink.expandLocation(location);
-        //NOTE: this line throws an error in firefox, but it still works.
-        this._box.src = location;
+    setStyle: function(name, value) {
+        if (name != "backgroundColor") this.$.setStyle.apply(this, [name, value]);
     }
 });
 
