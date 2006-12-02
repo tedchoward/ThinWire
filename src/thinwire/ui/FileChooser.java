157b2d87ef4f077f35b95469be894dfa174bb23e
Joshua Gertzen <me@joshgertzen.com>
2006-12-02 21:25:40 +0000

    [12/02/06] * Updated the license headers of all source files to reflect the new licensing terms.

--- src/thinwire/ui/FileChooser.java
+++ src/thinwire/ui/FileChooser.java
@@ -1,28 +1,33 @@
 /*
-                         ThinWire(TM) RIA Ajax Framework
-               Copyright (C) 2003-2006 Custom Credit Systems
-  
-  This program is free software; you can redistribute it and/or modify it under
-  the terms of the GNU General Public License as published by the Free Software
-  Foundation; either version 2 of the License, or (at your option) any later
-  version.
-  
-  This program is distributed in the hope that it will be useful, but WITHOUT
-  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-  FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
-  
-  You should have received a copy of the GNU General Public License along with
-  this program; if not, write to the Free Software Foundation, Inc., 59 Temple
-  Place, Suite 330, Boston, MA 02111-1307 USA
- 
-  Users wishing to use this library in proprietary products which are not 
-  themselves to be released under the GNU Public License should contact Custom
-  Credit Systems for a license to do so.
+#IFNDEF ALT_LICENSE
+                           ThinWire(R) RIA Ajax Framework
+                 Copyright (C) 2003-2006 Custom Credit Systems
+
+  This library is free software; you can redistribute it and/or modify it under
+  the terms of the GNU Lesser General Public License as published by the Free
+  Software Foundation; either version 2.1 of the License, or (at your option) any
+  later version.
+
+  This library is distributed in the hope that it will be useful, but WITHOUT ANY
+  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+  PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public License along
+  with this library; if not, write to the Free Software Foundation, Inc., 59
+  Temple Place, Suite 330, Boston, MA 02111-1307 USA
+
+  Users who would rather have a commercial license, warranty or support should
+  contact the following company who invented, built and supports the technology:
   
                 Custom Credit Systems, Richardson, TX 75081, USA.
-                           http://www.thinwire.com
- #VERSION_HEADER#
- */
+   	            email: info@thinwire.com    ph: +1 (888) 644-6405
+ 	                        http://www.thinwire.com
+#ENDIF
+#IFDEF ALT_LICENSE
+#LICENSE_HEADER#
+#ENDIF
+#VERSION_HEADER#
+*/
 package thinwire.ui;
 
 import java.io.File;
