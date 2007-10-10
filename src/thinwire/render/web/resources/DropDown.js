640716df3f66c42c3998ccde641cd69881fe9041
Joshua Gertzen <me@joshgertzen.com>
2007-10-10 22:51:44 +0000

    * Merged bugfixes from 1.2 branch (r511)

--- src/thinwire/render/web/resources/DropDown.js
+++ src/thinwire/render/web/resources/DropDown.js
@@ -18,7 +18,7 @@
 
   Users who would rather have a commercial license, warranty or support should
   contact the following company who invented, built and supports the technology:
-  
+
                 Custom Credit Systems, Richardson, TX 75081, USA.
                 email: info@thinwire.com    ph: +1 (888) 644-6405
                             http://www.thinwire.com
@@ -36,40 +36,43 @@ var tw_DropDown = tw_BaseText.extend({
     _buttonWidth: 16,
     _focusBorderSize: 0,
     _buttonBorderSize: 0,
-    
+
     construct: function(id, containerId, props) {
         var button = this._button = document.createElement("div");
         var buttonBorder = this._buttonBorder = document.createElement("div");
         arguments.callee.$.call(this, ["div", "input", "text"], "dropDownGridBox", id, containerId);
-		//Hack to work around IE height sizing issue (font-size: 1px;)
-		tw_Component.setCSSText("position:absolute;overflow:hidden;padding:0px;margin:0px;font-size:1px;", this._box);
+        //Hack to work around IE height sizing issue (font-size: 1px;)
+        tw_Component.setCSSText("position:absolute;overflow:hidden;padding:0px;margin:0px;font-size:1px;", this._box);
         this._subtractEditorWidth += this._buttonWidth;
-        
+
         var s = button.style;
-		var cssText = "background-repeat:no-repeat;background-position:center center;background-color:" + tw_COLOR_BUTTONFACE + ";";
-		tw_Component.setCSSText(cssText, button);
+        var cssText = "background-repeat:no-repeat;background-position:center center;background-color:" + tw_COLOR_BUTTONFACE + ";";
+        tw_Component.setCSSText(cssText, button);
         tw_Component.applyButtonBorder(button);
-        
+
         var s = buttonBorder.style;
-		cssText = "position:absolute;right:0px;background-color:" + tw_COLOR_TRANSPARENT + ";border-style:solid;border-color:" + 
-			tw_COLOR_WINDOWFRAME + ";border-width:0px;";
-		tw_Component.setCSSText(cssText, buttonBorder);
+        cssText = "position:absolute;right:0px;background-color:" + tw_COLOR_TRANSPARENT + ";border-style:solid;border-color:" +
+            tw_COLOR_WINDOWFRAME + ";border-width:0px;";
+        tw_Component.setCSSText(cssText, buttonBorder);
         buttonBorder.appendChild(button);
         this._box.appendChild(buttonBorder);
-                
+
         tw_addEventListener(this._box, "focus", this._focusListener);
         tw_addEventListener(button, "focus", this._focusListener);
         tw_addEventListener(this._box, "blur", this._blurListener);
         tw_addEventListener(button, "blur", this._blurListener);
-            
+
         tw_addEventListener(button, "mousedown", this._buttonMouseDownListener.bind(this));
         tw_addEventListener(button, ["mouseup", "mouseout"], this._buttonMouseUpListener.bind(this));
         this._buttonClickListener = this._buttonClickListener.bind(this);
-        tw_addEventListener(button, "click", this._buttonClickListener); 
+        tw_addEventListener(button, "click", this._buttonClickListener);
+
+        this._parentScrollListener = this._parentScrollListener.bind(this);
+        tw_addEventListener(this._parent._container, "scroll", this._parentScrollListener);
 
         this.init(-1, props);
     },
-    
+
     setComponent: function(compId) {
         var comp = tw_Component.instances[compId];
         var showDropDown = false;
@@ -83,34 +86,34 @@ var tw_DropDown = tw_BaseText.extend({
 
         this._ddComp.setCompVisible = this._ddComp.setVisible;
         this._ddComp.setVisible = this.setDropDownVisible.bind(this);
-        
-        this._ddComp.setVisible(showDropDown);  
+
+        this._ddComp.setVisible(showDropDown);
     },
-    
+
     addCloseComponent: function(compId) {
         var comp = tw_Component.instances[compId];
         comp._dropDownId = this._id;
         comp.fireCompAction = comp.fireAction;
         comp.fireAction = this._fireCloseAction;
     },
-    
+
     _fireCloseAction: function(subType, action, data) {
         if (action == "click") tw_Component.instances[this._dropDownId]._ddComp.setVisible(false);
         this.fireCompAction(subType, action, data);
     },
-    
+
     _buttonMouseDownListener: function(ev) {
-        if (!this._enabled || tw_getEventButton(ev) != 1) return; 
+        if (!this._enabled || tw_getEventButton(ev) != 1) return;
         var s = this._button.style;
         s.borderStyle = "solid";
         var pad = this._buttonBorderSize;
         var width = Math.floor(pad / 2);
         pad -= width;
-        s.borderWidth = width + "px"; 
+        s.borderWidth = width + "px";
         s.borderColor = tw_COLOR_THREEDSHADOW;
         s.padding = pad + "px";
     },
-        
+
     _buttonMouseUpListener: function(ev) {
         if (this._enabled && tw_getEventButton(ev) == 1 || ev.type == "mouseout") {
             var s = this._button.style;
@@ -119,10 +122,10 @@ var tw_DropDown = tw_BaseText.extend({
             s.padding = "0px";
         }
     },
-    
+
     _buttonClickListener: function(ev) {
         if (!this._enabled || this._ddComp == null) return;
-        
+
         if (!this._ddComp.isVisible()) {
             if (this._focusCapable) this.setFocus(true);
             this.setDropDownVisible(true);
@@ -130,29 +133,35 @@ var tw_DropDown = tw_BaseText.extend({
             this.setDropDownVisible(false);
         }
     },
-    
+
+    _parentScrollListener: function(ev) {
+        if (this._ddComp.isVisible()) {
+            this._ddComp.setVisible(false);
+        }
+    },
+
     setWidth: function(width) {
         arguments.callee.$.call(this, width);
         width = this._buttonWidth;
-        width -= this._focusBorderSize * 2;        
+        width -= this._focusBorderSize * 2;
         if (width < 0) width = 0;
         this._buttonBorder.style.width = width + "px";
         width -= this._buttonBorderSize * 2;
-        if (width < 0) width = 0;        
-        this._button.style.width = width + "px"; 
+        if (width < 0) width = 0;
+        this._button.style.width = width + "px";
     },
 
     setHeight: function(height) {
         arguments.callee.$.call(this, height);
         height -= this._borderSizeSub;
         height -= this._focusBorderSize * 2;
-        if (height < 0) height = 0;        
+        if (height < 0) height = 0;
         this._buttonBorder.style.height = height + "px";
         height -= this._buttonBorderSize * 2;
-        if (height < 0) height = 0;        
-        this._button.style.height = height + "px"; 
+        if (height < 0) height = 0;
+        this._button.style.height = height + "px";
     },
-    
+
     setVisible: function(visible) {
         arguments.callee.$.call(this, visible);
         if (!visible) this.setDropDownVisible(false);
@@ -163,41 +172,41 @@ var tw_DropDown = tw_BaseText.extend({
         this._button.style.backgroundImage = "url(" + (enabled ? tw_IMAGE_DROPDOWN_BUTTONARROW : tw_IMAGE_DROPDOWN_BUTTONARROWDISABLED) + ")";
         if (enabled && !this._editAllowed) this._editor.readOnly = true;
     },
-            
-    setFocus: function(focus) {          
+
+    setFocus: function(focus) {
         if (!this._enabled || !this.isVisible()) return false;
-        
+
         if (!focus && this._ddComp != null) {
             var active = tw_getActiveElement();
-            
+
             if (active != null) {
                 while (active.id <= 0) active = active.parentNode;
                 if (active.id == this._ddComp._id) return false;
             }
             this._ddComp.setVisible(false);
         }
-        
-        this._setFocusStyle(focus);        
+
+        this._setFocusStyle(focus);
         return arguments.callee.$.call(this, focus);
     },
 
     setStyle: function(name, value) {
         if (name == "borderWidth") {
-            this._buttonBorderSize = parseInt(value) >= 2 ? 2 : 1;
+            this._buttonBorderSize = parseInt(value, 10) >= 2 ? 2 : 1;
             this._button.style.borderWidth = this._buttonBorderSize + "px";
         }
-        
+
         arguments.callee.$.call(this, name, value);
     },
-    
+
     keyPressNotify: function(keyPressCombo) {
-        if (this._ddComp == null) return;        
+        if (this._ddComp == null) return;
         if (!this._ddComp.isVisible()) {
             if (keyPressCombo == "ArrowDown") {
                 this.setDropDownVisible(true);
-                return false;                
+                return false;
             } else {
-                return arguments.callee.$.call(this, keyPressCombo);                
+                return arguments.callee.$.call(this, keyPressCombo);
             }
         } else {
             if (keyPressCombo == "Esc" || keyPressCombo == "Enter") {
@@ -210,8 +219,8 @@ var tw_DropDown = tw_BaseText.extend({
             }
         }
     },
-    
-    setEditAllowed: function(editAllowed) {        
+
+    setEditAllowed: function(editAllowed) {
         this._editAllowed = editAllowed;
         if (this._enabled) this.setEnabled(true); //This will trigger the proper editAllowed state.
 
@@ -224,7 +233,7 @@ var tw_DropDown = tw_BaseText.extend({
             this._editor.style.cursor = "default";
         }
     },
-    
+
     _setFocusStyle: function(state) {
         if (this._focusBorderSize == state) return;
         this._focusBorderSize = state ? 1 : 0;
@@ -235,9 +244,9 @@ var tw_DropDown = tw_BaseText.extend({
 
     setDropDownVisible: function(state) {
         if (this._ddComp == null) return;
-        
+
         if (state) {
-            var parent = this._parent; 
+            var parent = this._parent;
             var offsetX = this._x;
             var offsetY = this._y;
 
@@ -246,14 +255,14 @@ var tw_DropDown = tw_BaseText.extend({
                 offsetY += parent._y + parent.getOffsetY();
                 parent = parent._parent;
             }
-            
+
             offsetX += parent.getOffsetX();
             offsetY += parent.getOffsetY();
-            
+
             var comp = this._ddComp;
             var availableHeight = tw_getVisibleHeight() - comp._box.parentNode.offsetTop - offsetY - this._height;
             offsetY += (availableHeight < comp._height ? -comp._height : this._height);
-            
+
             comp.setY(offsetY);
             comp.setX(offsetX);
             comp.setCompVisible(true);
@@ -264,8 +273,9 @@ var tw_DropDown = tw_BaseText.extend({
             if (this._focusCapable) this.setFocus(true);
         }
     },
-        
-    destroy: function() {        
+
+    destroy: function() {
+        tw_removeEventListener(this._parent._container, "scroll", this._parentScrollListener);
         this._ddComp.destroy();
         this._ddComp = this._button = this._buttonBorder = null;
         arguments.callee.$.call(this);
