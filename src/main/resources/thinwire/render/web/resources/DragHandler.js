13d9a6217bcfbd88a0f0a297c4a04a393804ea57
Ted C. Howard <ted@tedchoward.com>
2012-04-12 12:45:35 -0500

    Updated license header
    The old header referenced ThinWire LLC, which was a dream that never
    fully materialized. The copyright on the code and the ThinWire
    trademark are owned by Custom Credit Systems.

--- src/main/resources/thinwire/render/web/resources/DragHandler.js
+++ src/main/resources/thinwire/render/web/resources/DragHandler.js
@@ -1,28 +1,20 @@
 /*
-                           ThinWire(R) Ajax RIA Framework
-                        Copyright (C) 2003-2008 ThinWire LLC
+ThinWire(R) Ajax RIA Framework
+Copyright (C) 2003-2008 Custom Credit Systems
 
-  This library is free software; you can redistribute it and/or modify it under
-  the terms of the GNU Lesser General Public License as published by the Free
-  Software Foundation; either version 2.1 of the License, or (at your option) any
-  later version.
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
 
-  This library is distributed in the hope that it will be useful, but WITHOUT ANY
-  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
-  PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
 
-  You should have received a copy of the GNU Lesser General Public License along
-  with this library; if not, write to the Free Software Foundation, Inc., 59
-  Temple Place, Suite 330, Boston, MA 02111-1307 USA
-
-  Users who would rather have a commercial license, warranty or support should
-  contact the following company who supports the technology:
-  
-            ThinWire LLC, 5919 Greenville #335, Dallas, TX 75206-1906
-   	            email: info@thinwire.com    ph: +1 (214) 295-4859
- 	                        http://www.thinwire.com
-
-#VERSION_HEADER#
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 */
 var tw_DragHandler = Class.extend({
     _box: null,
